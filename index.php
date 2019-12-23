27bb0323d4a3eb17dcfba6658737baad67cd27ba
Nematzhon <49225146+Nematzhon@users.noreply.github.com>
2019-12-23 23:57:52 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Qazaq Republic</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/script.js"></script>
+</head>
+
+<body>
+	<header>
+		<div>
+			<a href="" class="header_menu">
+				FREE SHIPPING ON ALL KZ ORDERS OVER $50
+			</a>
+		</div>
+		<div>
+					<a href="index.php" class="header_menu">
+				QR
+			</a>	
+			<a href="products.html" class="header_menu">
+				Products
+			</a>
+            <a href="about.html" class="header_menu">
+				About QR
+			</a>
+			<a href="" class="header_menu">
+				Wish List
+			</a>
+			<a href="login.html" class="header_menu">
+				My Account
+			</a>
+		</div>
+
+	</header>
+
+
+	<section class="finder" style="padding-bottom: 20px;">
+		<img src="images/logo.png" id="logo">
+<div class="wrapper">
+  <div class="container-search">
+    <div class="container-input">
+      <button class="set_search">
+      <span class="fa fa-search" >
+      </span>
+      </button>
+      <input type="text" list="psychology_theories_list" id="psychology_theories_search" class="search-term" placeholder="Search">
+   
+      <button type="submit" class="search-button" id="selectTheory">
+         
+        <i >search</i>
+     </button>
+     </div>
+     
+     </div>
+     </div>
+
+	</section>
+	
+	<section class="slider_section">
+		<div class="slider" id="main-slider"><!-- outermost container element -->
+			<div class="slider-wrapper" id='image'><!-- innermost wrapper element -->
+				photo
+			</div>
+		</div>	
+
+
+
+
+
+	</section>
+	<section class="section_products" >
+		<div class="products_list_q">
+		
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(1).jpeg">
+				<a href="product_page.html" "name_product" href="">Product 1</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(2).jpeg">
+				<a class="name_product" href="">Product 2</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(3).jpeg">
+				<a class="name_product" href="">Product 3</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(4).jpeg">
+				<a class="name_product" href="">Name of Product 4</a>
+				<p>$39.00</p>
+			</div>
+		
+			
+		
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F.jpeg">
+				<a class="name_product" href="">Product 1</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(1).jpeg">
+				<a class="name_product" href="">Product 2</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(2).jpeg">
+				<a class="name_product" href="">Product 3</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(4).jpeg">
+				<a class="name_product" href="">Product 4</a>
+				<p>$39.00</p>
+			</div>
+						<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(1).jpeg">
+				<a href="product_page.html" "name_product" href="">Product 1</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(2).jpeg">
+				<a class="name_product" href="">Product 2</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(3).jpeg">
+				<a class="name_product" href="">Product 3</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="images/%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%20(4).jpeg">
+				<a class="name_product" href="">Name of Product 4</a>
+				<p>$39.00</p>
+			</div>
+		
+			
+		</div>
+
+
+
+	</section>
+
+
+  
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/about/">About Us</a></li>
+              <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+         <a href="#">Scanfcode</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+</footer>
+
+</body>
+</html>
+
+
+
---
+++ jso.php
@@ -0,0 +1,9 @@
+<?php
+$myObj->name = "Tshirt";
+$myObj->description = white;
+$myObj->image = "gfdjgerhjkgerg";
+$myObj->price = "20";
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
